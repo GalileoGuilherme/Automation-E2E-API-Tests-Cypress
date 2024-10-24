a4c0218e0d9bb7e364de30e4bdcd864786306eb0
GalileoGuilherme <76979588+GalileoGuilherme@users.noreply.github.com>
2024-10-24 14:17:25 -0300

    ajust

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,35 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// cypress/support/commands.js
+
+Cypress.Commands.add('apiRequest', (method, url, body = null) => {
+    return cy.request({
+      method,
+      url,
+      headers: { 'Content-Type': 'application/json' },
+      body,
+      failOnStatusCode: false, // evita que o teste falhe automaticamente
+    });
+  });
+  
+  Cypress.Commands.add('logResponse', (response) => {
+    const responseBody = response.body;
+  
+    // Verifica se a resposta está em formato JSON
+    if (typeof responseBody === 'string') {
+      try {
+        const parsedBody = JSON.parse(responseBody);
+        cy.log(JSON.stringify(parsedBody, null, 2));
+        return parsedBody;
+      } catch (e) {
+        cy.log('Erro ao parsear a resposta como JSON: ', e.message);
+        return responseBody;
+      }
+    } else {
+      cy.log(JSON.stringify(responseBody, null, 2));
+      return responseBody;
+    }
+  });
+  

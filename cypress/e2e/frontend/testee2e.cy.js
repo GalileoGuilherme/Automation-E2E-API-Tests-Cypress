c7e976442494be36dfab0485239c82da06e47794
GalileoGuilherme <76979588+GalileoGuilherme@users.noreply.github.com>
2024-11-13 18:29:26 -0300

    TesteCase1_E2E

--- cypress/e2e/frontend/testee2e.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe('Exemplo de Teste', () => {
-    it('Deve carregar a página inicial', () => {
-      cy.visit('/');
-      cy.contains('Automation Exercise'); // Verifica se o texto "Automation Exercise" está na página inicial
-    });
-  });
-  
